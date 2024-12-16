01a8057391c9e43a918c81ce205a2d871c905ba5
Rasika <rasikamekhe@gmail.com>
2024-12-16 07:22:40 +0530

    student.cpp

--- student.cpp
+++ student.cpp
@@ -5,6 +5,7 @@ allocation operatorsnew and delete as well as exception*/
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class studdata;
@@ -12,8 +13,8 @@ class studdata;
 class student {
     string name, Class, dob;
     int roll_no;
-    char* div;
-    char* bloodgroup;
+    string div;  // Use string instead of char*
+    string bloodgroup;  // Use string instead of char*
     static int count;
 
 public:
@@ -22,21 +23,22 @@ public:
         name = " ";
         roll_no = 0;
         Class = " ";
-        div = new char[50]; // Allocate enough space for division
+        div = "";
         dob = "dd/mm/yyyy";
-        bloodgroup = new char[5]; // Allocate space for blood group
+        bloodgroup = "";
     }
 
     // Destructor
     ~student() {
-        delete[] div; // Delete the array allocated for div
-        delete[] bloodgroup; // Delete the array allocated for bloodgroup
+        // No need for explicit deletion as strings manage their own memory
     }
 
+    // Static function to return student count
     static int getcount() {
         return count;
     }
 
+    // Member functions
     void getdata(studdata* st);
     void displaydata(studdata* st1);
 };
@@ -46,101 +48,115 @@ int student::count = 0;
 
 class studdata {
     string address;
-    long* telno;
-    long* drivinglicense;
-    friend class student; // Using friend function
+    long telno;
+    long drivinglicense;
+    friend class student;  // Friend class allows access to private members
 
 public:
     // Default constructor
     studdata() {
         address = " ";
-        telno = new long; // Using new operator
-        drivinglicense = new long; // Using new operator
+        telno = 0;
+        drivinglicense = 0;
     }
 
     // Destructor
     ~studdata() {
-        delete telno; // Using delete operator
-        delete drivinglicense; // Using delete operator
+        // No dynamically allocated memory, so no explicit cleanup required
     }
 
+    // Input and display functions
     void getstuddata() {
         cout << "Enter Address of student: ";
-        cin.ignore(); // Clear the newline character from previous input
-        getline(cin, address); // Using getline to get proper address with spaces
+        cin.ignore();  // Clear the newline character from previous input
+        getline(cin, address);  // Get address with spaces
         cout << "Enter telephone no. of the student: ";
-        cin >> *telno;
+        cin >> telno;
         cout << "Enter driving license no. of student: ";
-        cin >> *drivinglicense;
+        cin >> drivinglicense;
     }
 
-    void displaystuddata() {
+    void displaystuddata() const {
         cout << "Student address: " << address << endl;
-        cout << "Student's telephone no.: " << *telno << endl;
-        cout << "Student's driving license no.: " << *drivinglicense << endl;
+        cout << "Student's telephone no.: " << telno << endl;
+        cout << "Student's driving license no.: " << drivinglicense << endl;
     }
 };
 
+// Member function definitions for student class
 void student::getdata(studdata* st) {
     cout << "Enter student's name: ";
-    cin.ignore(); // Clear the newline character
+    cin.ignore();  // Clear the newline character
     getline(cin, name);
     cout << "Enter roll no. of student: ";
     cin >> roll_no;
+    cin.ignore();
     cout << "Enter the class of student: ";
-    cin.ignore(); // Clear the newline character
     getline(cin, Class);
     cout << "Enter the division of student: ";
-    cin >> div; // Read directly into div
-    cout << "Enter DOB of student: ";
-    cin.ignore(); // Clear the newline character
+    getline(cin, div);
+    cout << "Enter DOB of student (dd/mm/yyyy): ";
     getline(cin, dob);
     cout << "Enter the blood group of student: ";
-    cin >> bloodgroup; // Read directly into bloodgroup
-    st->getstuddata(); // Using this pointer to get the data of class studdata
+    getline(cin, bloodgroup);
+
+    // Call method of studdata to input additional details
+    st->getstuddata();
+
+    // Increment the static student count
     count++;
 }
 
 void student::displaydata(studdata* st1) {
-    cout << "Name of student is: " << name << endl;
-    cout << "Roll no. of student is: " << roll_no << endl;
-    cout << "Class of student is: " << Class << endl;
-    cout << "Division of student is: " << div << endl;
-    cout << "Date of birth of student is: " << dob << endl;
-    cout << "Blood group of student is: " << bloodgroup << endl;
-    st1->displaystuddata(); // Using this pointer to display data of class studdata
+    cout << "\nName of student: " << name;
+    cout << "\nRoll no. of student: " << roll_no;
+    cout << "\nClass of student: " << Class;
+    cout << "\nDivision of student: " << div;
+    cout << "\nDate of birth of student: " << dob;
+    cout << "\nBlood group of student: " << bloodgroup << endl;
+
+    // Call display function of studdata
+    st1->displaystuddata();
 }
 
 int main() {
-    student* stud1[100];
-    studdata* stud2[100];
-    int n = 0;
+    vector<student*> stud1;  // Use vector for dynamic student storage
+    vector<studdata*> stud2;
     char ch;
 
     do {
-        stud1[n] = new student;
-        stud2[n] = new studdata;
+        // Allocate new student and studdata dynamically
+        student* s = new student;
+        studdata* d = new studdata;
+
+        // Input student details
         cout << ".........ENTER STUDENT DATA.............." << endl;
-        stud1[n]->getdata(stud2[n]);
-        n++;
-        cout << "Do you want to add another student? press y for yes and n for no: ";
+        s->getdata(d);
+
+        // Add pointers to vectors
+        stud1.push_back(s);
+        stud2.push_back(d);
+
+        // Ask if the user wants to add another student
+        cout << "Do you want to add another student? (y/n): ";
         cin >> ch;
-        cin.ignore(); // Clear the newline character
+
     } while (ch == 'y' || ch == 'Y');
 
-    for (int i = 0; i < n; i++) {
+    // Display all students
+    for (size_t i = 0; i < stud1.size(); ++i) {
         cout << ".............STUDENT DATABASE............." << endl;
         stud1[i]->displaydata(stud2[i]);
     }
-    
-    cout << "...........................";
+
+    cout << "..........................." << endl;
     cout << "Total students: " << student::getcount() << endl;
 
-    // Cleanup
-    for (int i = 0; i < n; i++) {
+    // Cleanup dynamically allocated memory
+    for (size_t i = 0; i < stud1.size(); ++i) {
         delete stud1[i];
         delete stud2[i];
     }
+
     return 0;
 }
-
